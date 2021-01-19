4c8f2ed833a341381116029854c2a04db74d6fbf
markmps <cph-ms845@cphbusiness.dk>
2021-01-19 06:48:23 +0100

    resource quick fix

--- src/main/java/rest/AktivitetResource.java
+++ src/main/java/rest/AktivitetResource.java
@@ -39,7 +39,8 @@ public class AktivitetResource {
         
         return "test";
     }
-   
+    
+    @Path("/aktiviteter")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAktiviteter() throws IOException {
